aea728aa978eb4cb7ce6bd36429cab5e96640f6c
Vimal <krvimal111@gmail.com>
2026-07-12 10:45:06 +0530

    Initial commit

---
+++ src/test/java/com/manvkris/springbootwebtutorial/SpringbootwebtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manvkris.springbootwebtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebtutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
